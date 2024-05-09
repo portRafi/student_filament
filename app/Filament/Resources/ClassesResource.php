422d90eff83b9ee63fbc36b40e7501cf12755994
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-05-10 00:05:54 +0700

    feat: grouping menu

--- app/Filament/Resources/ClassesResource.php
+++ app/Filament/Resources/ClassesResource.php
@@ -19,6 +19,8 @@ class ClassesResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Academic Management';
+
     public static function form(Form $form): Form
     {
         return $form
