6a006454a62fe4b842a1fb2da3a386d2af92b132
rafi <srafi8935@gmail.com.com>
2024-09-23 13:11:53 +0700

    profiledone

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,11 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Joaopaulolndev\FilamentEditProfile\FilamentEditProfilePlugin;
+use App\Livewire\CustomProfileComponent;
+use Joaopaulolndev\FilamentEditProfile\Pages\EditProfilePage;
+use Filament\Navigation\MenuItem;
+
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -54,6 +59,30 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                FilamentEditProfilePlugin::make()
+                ->slug('my-profile')
+                ->setTitle('My Profile')
+                ->setNavigationLabel('My Profile')
+                ->setNavigationGroup('Group Profile')
+                ->setIcon('heroicon-o-user')
+                ->setSort(10)
+                ->canAccess(fn () => auth()->user()->id === 1)
+                ->shouldRegisterNavigation(false)
+                ->shouldShowDeleteAccountForm(false)
+                ->shouldShowSanctumTokens()
+                ->shouldShowBrowserSessionsForm()
+                ->shouldShowAvatarForm()
+                ->customProfileComponents([
+                    \App\Livewire\CustomProfileComponent::class,
+                ])
+            ])
+            ->userMenuItems([
+                'profile' => MenuItem::make()
+                ->label(fn() => auth()->user()->name)
+                ->url(fn (): string => EditProfilePage::getUrl())
+                ->icon('heroicon-m-user-circle')
             ]);
     }
 }
