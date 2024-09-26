4a7c3a05d6397126ac0c1df1a66bf0ded0127c56
rafi <srafi8935@gmail.com.com>
2024-09-26 11:01:59 +0700

    icon

--- app/Filament/Resources/ClassesResource.php
+++ app/Filament/Resources/ClassesResource.php
@@ -17,7 +17,7 @@ class ClassesResource extends Resource
 {
     protected static ?string $model = Classes::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-percent-badge';
 
     protected static ?string $navigationGroup = 'Academic Management';
 
