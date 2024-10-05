8ff2305d725e3e37c605322e3f718afd39393a9c
rafi <srafi8935@gmail.com.com>
2024-10-05 08:44:31 +0700

    delete bulk delet

--- app/Filament/Resources/ClassesResource.php
+++ app/Filament/Resources/ClassesResource.php
@@ -50,7 +50,6 @@ class ClassesResource extends Resource
             ->actions([
                 
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
